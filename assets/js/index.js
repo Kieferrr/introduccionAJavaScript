bfa446e1f9c39f9279fb6ab1f7b4435a12bfdeb0
Nicolás <nicog.rivero@gmail.com>
2025-04-19 17:20:01 -0400

    Primer commit

---
+++ assets/js/index.js
@@ -0,0 +1,15 @@
+precio = 400000;
+
+precioSpan = document.querySelector(".precio-inicial");
+precioSpan.innerHTML = precio;
+
+let precioTotal = 10
+
+precioAPagar = document.querySelector(".valor-total");
+
+
+
+precioAPagar.innerHTML = precioTotal;
+
+
+
